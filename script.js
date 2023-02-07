6d693ca617165391a29534507adb30dbad7898ca
Julian4806 <210549@student.glu.nl>
2023-02-07 15:40:54 +0100

    added working "Sign Up with Email" button

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-!
+const toggleButton = document.querySelector('.email')
+const form = document.querySelector('form')
+
+toggleButton.addEventListener('click', () => {
+    form.style.display === "none" ? form.style.display = "flex" : form.style.display = "none"
+})
